8039ac94c815fd7869f277604f0c9ef28dd2d9f4
John Wregglesworth <wregglej@cyverse.org>
2021-03-15 14:50:44 -0700

    Add ids and i18n for instant launches in the dashboard

--- src/components/dashboard/ids.js
+++ src/components/dashboard/ids.js
@@ -11,4 +11,5 @@ export default {
     SECTION_NEWS: "section.news",
     SECTION_EVENTS: "section.events",
     SECTION_VIDEOS: "section.videos",
+    SECTION_INSTANT_LAUNCHES: "section.instant-launches",
 };
